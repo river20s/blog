f9c32e6ac34d66dc1c3a36877763f2709ae6ef78
Eun-Been Kang <river20s@naver.com>
2024-10-09 21:06:43 +0900

    10월 9일 작업내용 정리

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+
+// 메뉴 열기
+menuIcon.addEventListener('mouseenter', function() {
+    navLinks.classList.add('active');
+});
+
+// 메뉴 닫기
+navLinks.addEventListener('mouseleave', function() {
+    navLinks.classList.remove('active');
+});
